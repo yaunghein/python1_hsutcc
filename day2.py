cc936f0fb23737c718673729bb570425eabc9fcd
Yan Aung Hein <yan@apmhelp.com>
2025-10-23 13:09:46 +0700

    till day 3

---
+++ day2.py
@@ -0,0 +1,2 @@
+age = int(input("Enter your age: "))
+print(f"You are {age} years old.")
---
+++ day3.py
@@ -0,0 +1,21 @@
+# a = int(input())
+# print(a + 1)
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print(a + b + c)
+
+# a = int(input())
+# b = int(input())
+# print(a * b)
+
+# n = int(input())
+# m = int(input())
+# print(n % m)
+
+# a = input()
+# b = a.strip()
+# c = b[-1]
+# d = int(c)
+# print(d)
