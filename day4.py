f86d7a13dd8e64343b68ce9b64e79a95d2557d92
Yan Aung Hein <yan@apmhelp.com>
2025-10-24 02:30:08 +0700

    day4

--- day4.py
+++ day4.py
@@ -1,3 +1,41 @@
-celcius = input("Enter temperature in Celcius: ")
-fahrenheit = ((9 / 5) * float(celcius)) + 32
-print("Temperature in Fahrenheit is:", fahrenheit)
+# if (4 > 6):
+#     print("4 is greater than 6")
+# else:
+#     print("4 is not greater than 6")
+
+# flat_number = input("")
+# print(flat_number[0])
+
+# current_flowers = int(input(""))
+# days = int(input(""))
+# print(current_flowers * (3 ** days))
+
+# total_minutes = int(input(""))
+# hours = total_minutes // 60
+# minutes = total_minutes % 60
+# print(hours)
+# print(minutes)
+
+# temperature = int(input(""))
+# if temperature > 35:
+#     print("CANCEL SCHOOL")
+# elif temperature > 30:
+#     print("CANCEL TWO CLASSES")
+# else:
+#     print("NORMAL CLASSES")
+
+# coins_in_first_chest = int(input(""))
+# coins_in_second_chest = int(input(""))
+# coins_in_third_chest = int(input(""))
+# max_coins = max(
+#     coins_in_first_chest + coins_in_second_chest,
+#     coins_in_first_chest + coins_in_third_chest,
+#     coins_in_second_chest + coins_in_third_chest
+# )
+# print(max_coins)
+
+# number = int(input(""))
+# if (number % 7 == 0) and (number % 11 != 0):
+#     print("YES")
+# else:
+#     print("NO")
