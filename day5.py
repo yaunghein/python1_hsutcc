44c72998300b59f39d5bbf90146d99c29564e57b
Yan Aung Hein <yan@apmhelp.com>
2025-10-24 15:52:00 +0700

    print even number

--- day5.py
+++ day5.py
@@ -1,9 +1,14 @@
-x = float(input(""))
+# x = float(input(""))
 
-if (x % 2 == 0):
-    if (x % 3 != 0):
-        print("B")
-    else:
-        print("C")
-else:
-    print("D")
+# if (x % 2 == 0):
+#     if (x % 3 != 0):
+#         print("B")
+#     else:
+#         print("C")
+# else:
+#     print("D")
+
+number = 2
+while number <= 20:
+    print(number)
+    number += 2
