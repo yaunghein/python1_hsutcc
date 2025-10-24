024b5cf311315981b157edd4cdc853684589f04e
Yan Aung Hein <yan@apmhelp.com>
2025-10-24 14:32:45 +0700

    refactor

--- day5.py
+++ day5.py
@@ -1,14 +1,13 @@
-x = int(input(""))
+x = float(input(""))
 
 is_even = x % 2 == 0
 is_divisible_by_3 = x % 3 == 0
 is_not_divisible_by_3 = x % 3 != 0
 
-if (is_even and is_not_divisible_by_3):
-    print("B")
-elif (is_even and is_divisible_by_3):
-    print("C")
-elif (is_even):
-    print("A")
+if (is_even):
+    if (is_not_divisible_by_3):
+        print("B")
+    else:
+        print("C")
 else:
     print("D")
